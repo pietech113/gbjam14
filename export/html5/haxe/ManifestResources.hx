870b185e128a369208b6a26b4f8056a2e7138c64
Technomancer <krowizard113@gmail.com>
2026-09-13 20:26:12 -0700

    hello world

---
+++ export/html5/haxe/ManifestResources.hx
@@ -0,0 +1,195 @@
+package;
+
+import haxe.io.Bytes;
+import haxe.io.Path;
+import lime.utils.AssetBundle;
+import lime.utils.AssetLibrary;
+import lime.utils.AssetManifest;
+import lime.utils.Assets;
+
+#if sys
+import sys.FileSystem;
+#end
+
+#if disable_preloader_assets
+@:dox(hide) class ManifestResources {
+	public static var preloadLibraries:Array<Dynamic>;
+	public static var preloadLibraryNames:Array<String>;
+	public static var rootPath:String;
+
+	public static function init (config:Dynamic):Void {
+		preloadLibraries = new Array ();
+		preloadLibraryNames = new Array ();
+	}
+}
+#else
+@:access(lime.utils.Assets)
+
+
+@:keep @:dox(hide) class ManifestResources {
+
+
+	public static var preloadLibraries:Array<AssetLibrary>;
+	public static var preloadLibraryNames:Array<String>;
+	public static var rootPath:String;
+
+
+	public static function init (config:Dynamic):Void {
+
+		preloadLibraries = new Array ();
+		preloadLibraryNames = new Array ();
+
+		rootPath = null;
+
+		if (config != null && Reflect.hasField (config, "rootPath")) {
+
+			rootPath = Reflect.field (config, "rootPath");
+
+			if(!StringTools.endsWith (rootPath, "/")) {
+
+				rootPath += "/";
+
+			}
+
+		}
+
+		if (rootPath == null) {
+
+			#if (ios || tvos)
+			rootPath = "assets/";
+			#elseif android
+			rootPath = "";
+			#elseif (emscripten || webassembly)
+			rootPath = "";
+			#elseif (console || sys)
+			rootPath = lime.system.System.applicationDirectory;
+			#else
+			rootPath = "./";
+			#end
+
+		}
+
+		#if (openfl && !flash && !display)
+		openfl.text.Font.registerFont (__ASSET__OPENFL__flixel_fonts_nokiafc22_ttf);
+		openfl.text.Font.registerFont (__ASSET__OPENFL__flixel_fonts_monsterrat_ttf);
+		
+		#end
+
+		var data, manifest, library, bundle;
+
+		data = '{"name":null,"assets":"aoy4:pathy34:assets%2Fdata%2Fdata-goes-here.txty4:sizezy4:typey4:TEXTy2:idR1y7:preloadtgoR0y32:assets%2Fdata%2Fhatidea.asepriteR2i388R3y6:BINARYR5R7R6tgoR0y34:assets%2Fdata%2FmapScreen.asepriteR2i1365R3R8R5R9R6tgoR0y36:assets%2Fdata%2FPlaceholder.asepriteR2i345R3R8R5R10R6tgoR0y36:assets%2Fdata%2FplayerFront.asepriteR2i350R3R8R5R11R6tgoR0y40:assets%2Fdata%2FplayerFrontWalk.asepriteR2i390R3R8R5R12R6tgoR0y35:assets%2Fdata%2FPlayerFrontWalk.pngR2i293R3y5:IMAGER5R13R6tgoR0y35:assets%2Fdata%2FplayerSide.asepriteR2i345R3R8R5R15R6tgoR0y42:assets%2Fdata%2FplayerSideWalking.asepriteR2i422R3R8R5R16R6tgoR0y32:assets%2Fdata%2FtextBox.asepriteR2i521R3R8R5R17R6tgoR0y33:assets%2Fdata%2FtownIcon.asepriteR2i420R3R8R5R18R6tgoR0y39:assets%2Fdata%2FtownIconActive.asepriteR2i423R3R8R5R19R6tgoR0y36:assets%2Fimages%2Fimages-go-here.txtR2zR3R4R5R20R6tgoR0y31:assets%2Fimages%2FmapScreen.pngR2i1407R3R14R5R21R6tgoR0y33:assets%2Fimages%2Fplaceholder.pngR2i266R3R14R5R22R6tgoR0y33:assets%2Fimages%2FplayerFront.pngR2i250R3R14R5R23R6tgoR0y37:assets%2Fimages%2FPlayerFrontWalk.pngR2i293R3R14R5R24R6tgoR0y32:assets%2Fimages%2FplayerSide.pngR2i246R3R14R5R25R6tgoR0y39:assets%2Fimages%2FplayerSideWalking.pngR2i321R3R14R5R26R6tgoR0y29:assets%2Fimages%2FtextBox.pngR2i313R3R14R5R27R6tgoR0y30:assets%2Fimages%2FtownIcon.pngR2i350R3R14R5R28R6tgoR0y36:assets%2Fimages%2FtownIconActive.pngR2i343R3R14R5R29R6tgoR0y36:assets%2Fmusic%2Fmusic-goes-here.txtR2zR3R4R5R30R6tgoR0y36:assets%2Fsounds%2Fsounds-go-here.txtR2zR3R4R5R31R6tgoR2i8220R3y5:MUSICR5y26:flixel%2Fsounds%2Fbeep.mp3y9:pathGroupaR33y26:flixel%2Fsounds%2Fbeep.ogghR6tgoR2i39706R3R32R5y28:flixel%2Fsounds%2Fflixel.mp3R34aR36y28:flixel%2Fsounds%2Fflixel.ogghR6tgoR2i6840R3y5:SOUNDR5R35R34aR33R35hgoR2i33629R3R38R5R37R34aR36R37hgoR2i15744R3y4:FONTy9:classNamey35:__ASSET__flixel_fonts_nokiafc22_ttfR5y30:flixel%2Ffonts%2Fnokiafc22.ttfR6tgoR2i29724R3R39R40y36:__ASSET__flixel_fonts_monsterrat_ttfR5y31:flixel%2Ffonts%2Fmonsterrat.ttfR6tgoR0y33:flixel%2Fimages%2Fui%2Fbutton.pngR2i222R3R14R5R45R6tgoR0y36:flixel%2Fimages%2Flogo%2Fdefault.pngR2i484R3R14R5R46R6tgh","rootPath":null,"version":2,"libraryArgs":[],"libraryType":null}';
+		manifest = AssetManifest.parse (data, rootPath);
+		library = AssetLibrary.fromManifest (manifest);
+		Assets.registerLibrary ("default", library);
+		
+
+		library = Assets.getLibrary ("default");
+		if (library != null) preloadLibraries.push (library);
+		else preloadLibraryNames.push ("default");
+		
+
+	}
+
+
+}
+
+#if !display
+#if flash
+
+@:keep @:bind @:noCompletion #if display private #end class __ASSET__assets_data_data_goes_here_txt extends null { }
+@:keep @:bind @:noCompletion #if display private #end class __ASSET__assets_data_hatidea_aseprite extends null { }
+@:keep @:bind @:noCompletion #if display private #end class __ASSET__assets_data_mapscreen_aseprite extends null { }
+@:keep @:bind @:noCompletion #if display private #end class __ASSET__assets_data_placeholder_aseprite extends null { }
+@:keep @:bind @:noCompletion #if display private #end class __ASSET__assets_data_playerfront_aseprite extends null { }
+@:keep @:bind @:noCompletion #if display private #end class __ASSET__assets_data_playerfrontwalk_aseprite extends null { }
+@:keep @:bind @:noCompletion #if display private #end class __ASSET__assets_data_playerfrontwalk_png extends flash.display.BitmapData { public function new () { super (0, 0, true, 0); } }
+@:keep @:bind @:noCompletion #if display private #end class __ASSET__assets_data_playerside_aseprite extends null { }
+@:keep @:bind @:noCompletion #if display private #end class __ASSET__assets_data_playersidewalking_aseprite extends null { }
+@:keep @:bind @:noCompletion #if display private #end class __ASSET__assets_data_textbox_aseprite extends null { }
+@:keep @:bind @:noCompletion #if display private #end class __ASSET__assets_data_townicon_aseprite extends null { }
+@:keep @:bind @:noCompletion #if display private #end class __ASSET__assets_data_towniconactive_aseprite extends null { }
+@:keep @:bind @:noCompletion #if display private #end class __ASSET__assets_images_images_go_here_txt extends null { }
+@:keep @:bind @:noCompletion #if display private #end class __ASSET__assets_images_mapscreen_png extends flash.display.BitmapData { public function new () { super (0, 0, true, 0); } }
+@:keep @:bind @:noCompletion #if display private #end class __ASSET__assets_images_placeholder_png extends flash.display.BitmapData { public function new () { super (0, 0, true, 0); } }
+@:keep @:bind @:noCompletion #if display private #end class __ASSET__assets_images_playerfront_png extends flash.display.BitmapData { public function new () { super (0, 0, true, 0); } }
+@:keep @:bind @:noCompletion #if display private #end class __ASSET__assets_images_playerfrontwalk_png extends flash.display.BitmapData { public function new () { super (0, 0, true, 0); } }
+@:keep @:bind @:noCompletion #if display private #end class __ASSET__assets_images_playerside_png extends flash.display.BitmapData { public function new () { super (0, 0, true, 0); } }
+@:keep @:bind @:noCompletion #if display private #end class __ASSET__assets_images_playersidewalking_png extends flash.display.BitmapData { public function new () { super (0, 0, true, 0); } }
+@:keep @:bind @:noCompletion #if display private #end class __ASSET__assets_images_textbox_png extends flash.display.BitmapData { public function new () { super (0, 0, true, 0); } }
+@:keep @:bind @:noCompletion #if display private #end class __ASSET__assets_images_townicon_png extends flash.display.BitmapData { public function new () { super (0, 0, true, 0); } }
+@:keep @:bind @:noCompletion #if display private #end class __ASSET__assets_images_towniconactive_png extends flash.display.BitmapData { public function new () { super (0, 0, true, 0); } }
+@:keep @:bind @:noCompletion #if display private #end class __ASSET__assets_music_music_goes_here_txt extends null { }
+@:keep @:bind @:noCompletion #if display private #end class __ASSET__assets_sounds_sounds_go_here_txt extends null { }
+@:keep @:bind @:noCompletion #if display private #end class __ASSET__flixel_sounds_beep_mp3 extends null { }
+@:keep @:bind @:noCompletion #if display private #end class __ASSET__flixel_sounds_flixel_mp3 extends null { }
+@:keep @:bind @:noCompletion #if display private #end class __ASSET__flixel_sounds_beep_ogg extends null { }
+@:keep @:bind @:noCompletion #if display private #end class __ASSET__flixel_sounds_flixel_ogg extends null { }
+@:keep @:bind @:noCompletion #if display private #end class __ASSET__flixel_fonts_nokiafc22_ttf extends null { }
+@:keep @:bind @:noCompletion #if display private #end class __ASSET__flixel_fonts_monsterrat_ttf extends null { }
+@:keep @:bind @:noCompletion #if display private #end class __ASSET__flixel_images_ui_button_png extends flash.display.BitmapData { public function new () { super (0, 0, true, 0); } }
+@:keep @:bind @:noCompletion #if display private #end class __ASSET__flixel_images_logo_default_png extends flash.display.BitmapData { public function new () { super (0, 0, true, 0); } }
+@:keep @:bind @:noCompletion #if display private #end class __ASSET__manifest_default_json extends null { }
+
+
+#elseif (desktop || cpp)
+
+@:keep @:file("assets/data/data-goes-here.txt") @:noCompletion #if display private #end class __ASSET__assets_data_data_goes_here_txt extends haxe.io.Bytes {}
+@:keep @:file("assets/data/hatidea.aseprite") @:noCompletion #if display private #end class __ASSET__assets_data_hatidea_aseprite extends haxe.io.Bytes {}
+@:keep @:file("assets/data/mapScreen.aseprite") @:noCompletion #if display private #end class __ASSET__assets_data_mapscreen_aseprite extends haxe.io.Bytes {}
+@:keep @:file("assets/data/Placeholder.aseprite") @:noCompletion #if display private #end class __ASSET__assets_data_placeholder_aseprite extends haxe.io.Bytes {}
+@:keep @:file("assets/data/playerFront.aseprite") @:noCompletion #if display private #end class __ASSET__assets_data_playerfront_aseprite extends haxe.io.Bytes {}
+@:keep @:file("assets/data/playerFrontWalk.aseprite") @:noCompletion #if display private #end class __ASSET__assets_data_playerfrontwalk_aseprite extends haxe.io.Bytes {}
+@:keep @:image("assets/data/PlayerFrontWalk.png") @:noCompletion #if display private #end class __ASSET__assets_data_playerfrontwalk_png extends lime.graphics.Image {}
+@:keep @:file("assets/data/playerSide.aseprite") @:noCompletion #if display private #end class __ASSET__assets_data_playerside_aseprite extends haxe.io.Bytes {}
+@:keep @:file("assets/data/playerSideWalking.aseprite") @:noCompletion #if display private #end class __ASSET__assets_data_playersidewalking_aseprite extends haxe.io.Bytes {}
+@:keep @:file("assets/data/textBox.aseprite") @:noCompletion #if display private #end class __ASSET__assets_data_textbox_aseprite extends haxe.io.Bytes {}
+@:keep @:file("assets/data/townIcon.aseprite") @:noCompletion #if display private #end class __ASSET__assets_data_townicon_aseprite extends haxe.io.Bytes {}
+@:keep @:file("assets/data/townIconActive.aseprite") @:noCompletion #if display private #end class __ASSET__assets_data_towniconactive_aseprite extends haxe.io.Bytes {}
+@:keep @:file("assets/images/images-go-here.txt") @:noCompletion #if display private #end class __ASSET__assets_images_images_go_here_txt extends haxe.io.Bytes {}
+@:keep @:image("assets/images/mapScreen.png") @:noCompletion #if display private #end class __ASSET__assets_images_mapscreen_png extends lime.graphics.Image {}
+@:keep @:image("assets/images/placeholder.png") @:noCompletion #if display private #end class __ASSET__assets_images_placeholder_png extends lime.graphics.Image {}
+@:keep @:image("assets/images/playerFront.png") @:noCompletion #if display private #end class __ASSET__assets_images_playerfront_png extends lime.graphics.Image {}
+@:keep @:image("assets/images/PlayerFrontWalk.png") @:noCompletion #if display private #end class __ASSET__assets_images_playerfrontwalk_png extends lime.graphics.Image {}
+@:keep @:image("assets/images/playerSide.png") @:noCompletion #if display private #end class __ASSET__assets_images_playerside_png extends lime.graphics.Image {}
+@:keep @:image("assets/images/playerSideWalking.png") @:noCompletion #if display private #end class __ASSET__assets_images_playersidewalking_png extends lime.graphics.Image {}
+@:keep @:image("assets/images/textBox.png") @:noCompletion #if display private #end class __ASSET__assets_images_textbox_png extends lime.graphics.Image {}
+@:keep @:image("assets/images/townIcon.png") @:noCompletion #if display private #end class __ASSET__assets_images_townicon_png extends lime.graphics.Image {}
+@:keep @:image("assets/images/townIconActive.png") @:noCompletion #if display private #end class __ASSET__assets_images_towniconactive_png extends lime.graphics.Image {}
+@:keep @:file("assets/music/music-goes-here.txt") @:noCompletion #if display private #end class __ASSET__assets_music_music_goes_here_txt extends haxe.io.Bytes {}
+@:keep @:file("assets/sounds/sounds-go-here.txt") @:noCompletion #if display private #end class __ASSET__assets_sounds_sounds_go_here_txt extends haxe.io.Bytes {}
+@:keep @:file("C:/HaxeToolkit/haxe/lib/flixel/6,2,0/assets/sounds/beep.mp3") @:noCompletion #if display private #end class __ASSET__flixel_sounds_beep_mp3 extends haxe.io.Bytes {}
+@:keep @:file("C:/HaxeToolkit/haxe/lib/flixel/6,2,0/assets/sounds/flixel.mp3") @:noCompletion #if display private #end class __ASSET__flixel_sounds_flixel_mp3 extends haxe.io.Bytes {}
+@:keep @:file("C:/HaxeToolkit/haxe/lib/flixel/6,2,0/assets/sounds/beep.ogg") @:noCompletion #if display private #end class __ASSET__flixel_sounds_beep_ogg extends haxe.io.Bytes {}
+@:keep @:file("C:/HaxeToolkit/haxe/lib/flixel/6,2,0/assets/sounds/flixel.ogg") @:noCompletion #if display private #end class __ASSET__flixel_sounds_flixel_ogg extends haxe.io.Bytes {}
+@:keep @:font("export/html5/obj/webfont/nokiafc22.ttf") @:noCompletion #if display private #end class __ASSET__flixel_fonts_nokiafc22_ttf extends lime.text.Font {}
+@:keep @:font("export/html5/obj/webfont/monsterrat.ttf") @:noCompletion #if display private #end class __ASSET__flixel_fonts_monsterrat_ttf extends lime.text.Font {}
+@:keep @:image("C:/HaxeToolkit/haxe/lib/flixel/6,2,0/assets/images/ui/button.png") @:noCompletion #if display private #end class __ASSET__flixel_images_ui_button_png extends lime.graphics.Image {}
+@:keep @:image("C:/HaxeToolkit/haxe/lib/flixel/6,2,0/assets/images/logo/default.png") @:noCompletion #if display private #end class __ASSET__flixel_images_logo_default_png extends lime.graphics.Image {}
+@:keep @:file("") @:noCompletion #if display private #end class __ASSET__manifest_default_json extends haxe.io.Bytes {}
+
+
+
+#else
+
+@:keep @:expose('__ASSET__flixel_fonts_nokiafc22_ttf') @:noCompletion #if display private #end class __ASSET__flixel_fonts_nokiafc22_ttf extends lime.text.Font { public function new () { #if !html5 __fontPath = "flixel/fonts/nokiafc22"; #else ascender = 2048; descender = -512; height = 2816; numGlyphs = 172; underlinePosition = -640; underlineThickness = 256; unitsPerEM = 2048; #end name = "Nokia Cellphone FC Small"; super (); }}
+@:keep @:expose('__ASSET__flixel_fonts_monsterrat_ttf') @:noCompletion #if display private #end class __ASSET__flixel_fonts_monsterrat_ttf extends lime.text.Font { public function new () { #if !html5 __fontPath = "flixel/fonts/monsterrat"; #else ascender = 968; descender = -251; height = 1219; numGlyphs = 263; underlinePosition = -150; underlineThickness = 50; unitsPerEM = 1000; #end name = "Monsterrat"; super (); }}
+
+
+#end
+
+#if (openfl && !flash)
+
+#if html5
+@:keep @:expose('__ASSET__OPENFL__flixel_fonts_nokiafc22_ttf') @:noCompletion #if display private #end class __ASSET__OPENFL__flixel_fonts_nokiafc22_ttf extends openfl.text.Font { public function new () { __fromLimeFont (new __ASSET__flixel_fonts_nokiafc22_ttf ()); super (); }}
+@:keep @:expose('__ASSET__OPENFL__flixel_fonts_monsterrat_ttf') @:noCompletion #if display private #end class __ASSET__OPENFL__flixel_fonts_monsterrat_ttf extends openfl.text.Font { public function new () { __fromLimeFont (new __ASSET__flixel_fonts_monsterrat_ttf ()); super (); }}
+
+#else
+@:keep @:expose('__ASSET__OPENFL__flixel_fonts_nokiafc22_ttf') @:noCompletion #if display private #end class __ASSET__OPENFL__flixel_fonts_nokiafc22_ttf extends openfl.text.Font { public function new () { __fromLimeFont (new __ASSET__flixel_fonts_nokiafc22_ttf ()); super (); }}
+@:keep @:expose('__ASSET__OPENFL__flixel_fonts_monsterrat_ttf') @:noCompletion #if display private #end class __ASSET__OPENFL__flixel_fonts_monsterrat_ttf extends openfl.text.Font { public function new () { __fromLimeFont (new __ASSET__flixel_fonts_monsterrat_ttf ()); super (); }}
+
+#end
+
+#end
+#end
+
+#end
